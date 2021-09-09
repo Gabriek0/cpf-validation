f92d67e685a22ec2fb9b363ea1a58b6eecc6a317
Gabriek0 <gabriel.sanches170@gmail.com>
2021-09-09 20:49:23 -0300

    adicionando funcao de confirmar cpf

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ const inputName = document.querySelector(".input-name");
 const inputLabel = document.querySelector(".label-name");
 const inputCpf = document.querySelector(".input-cpf");
 const cpfLabel = document.querySelector(".label-cpf");
+const confirmationLabel = document.querySelector(".label-cpf-confirmation");
+const inputConfirmation = document.querySelector(".input-cpf-confirmation");
 
 function validationName() {
   inputName.addEventListener("change", (event) => {
@@ -33,7 +35,6 @@ function addDotsAndHifen(splittedLetters, cpfPattern) {
       }
     }
   });
-  console.log(cpfPattern.length);
 
   return cpfPattern;
 }
@@ -62,15 +63,41 @@ function validationInputCpf(value) {
 
 function validationCpf() {
   inputCpf.addEventListener("change", (event) => {
-    const img = document.querySelector(".img-check-cpf");
+    const imgCheck = document.querySelector(".img-check-cpf");
+    const imgNotCheck = document.querySelector(".img-not-check-cpf");
+
     let { value } = event.target;
 
     inputCpf.value = validationInputCpf(value);
 
-    if (value.length >= 14) {
-      img.style.display = "inline";
+    if (value.length >= 14 || value.length === 14) {
+      confirmationCpf(value);
+      imgCheck.style.display = "inline";
+      imgNotCheck.style.display = "none";
+    } else if (value.length > 1 && value.length < 14) {
+      imgNotCheck.style.display = "inline";
+      imgCheck.style.display = "none";
     } else {
-      img.style.display = "none";
+      imgCheck.style.display = "none";
+      imgNotCheck.style.display = "none";
+    }
+  });
+}
+
+function confirmationCpf(cpf) {
+  inputConfirmation.addEventListener("change", (event) => {
+    const imgCheck = document.querySelector(".img-check-cpf-bottom");
+    const imgNotCheck = document.querySelector(".img-not-check-cpf-bottom");
+
+    let { value } = event.target;
+    inputConfirmation.value = validationInputCpf(value);
+
+    if (value !== cpf) {
+      imgNotCheck.style.display = "inline";
+      imgCheck.style.display = "none";
+    } else {
+      imgCheck.style.display = "inline";
+      imgNotCheck.style.display = "none";
     }
   });
 }
